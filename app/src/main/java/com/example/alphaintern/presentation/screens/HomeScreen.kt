48d5e021bebbf29ae73260a20e262def68785fe1
Timur A <kh-g-2016@mail.ru>
2025-01-11 21:25:28 +0300

    fix: bnv is not visible

--- app/src/main/java/com/example/alphaintern/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/alphaintern/presentation/screens/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -22,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -47,6 +49,7 @@ fun CardInfoScreen(
     val loading by viewModel.loading.observeAsState(false)
     val errorMessage by viewModel.errorMessage.observeAsState()
     var binNumber by remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Column(
         modifier = Modifier
@@ -88,6 +91,7 @@ fun CardInfoScreen(
 
         Button(
             onClick = {
+                keyboardController?.hide()
                 if (binNumber.length in 6..8) {
                     viewModel.getCardInfo(binNumber)
                 }
