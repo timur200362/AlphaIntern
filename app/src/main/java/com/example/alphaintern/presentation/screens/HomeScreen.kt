6b070eb6c0877616bdc2ea7b152ea61e6321b32c
Timur <timurka.askarov@inbox.ru>
2025-01-11 04:05:24 +0300

    feat: ui 1st screen

--- app/src/main/java/com/example/alphaintern/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/alphaintern/presentation/screens/HomeScreen.kt
@@ -1,6 +1,11 @@
 package com.example.alphaintern.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -14,10 +19,13 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.alphaintern.presentation.mvvm.CardInfoViewModel
@@ -40,7 +48,15 @@ fun CardInfoScreen(
     val errorMessage by viewModel.errorMessage.observeAsState()
     var binNumber by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Spacer(modifier = Modifier.height(16.dp)) // Отступ сверху
+
         TextField(
             value = binNumber,
             onValueChange = {
@@ -50,7 +66,8 @@ fun CardInfoScreen(
             },
             label = { Text("Введите BIN номер (6-8 цифр)") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            isError = binNumber.length !in (6..8)
+            isError = binNumber.length !in (6..8),
+            modifier = Modifier.fillMaxWidth() // Полная ширина
         )
 
         Text(
@@ -60,36 +77,49 @@ fun CardInfoScreen(
             modifier = Modifier.padding(top = 4.dp)
         )
 
-        Button(onClick = {
-            if (binNumber.length in 6..8) {
-                viewModel.loadCardInfo(binNumber)
-            }
-        }) {
+        Button(
+            onClick = {
+                if (binNumber.length in 6..8) {
+                    viewModel.loadCardInfo(binNumber)
+                }
+            },
+            modifier = Modifier.padding(top = 2.dp)
+        ) {
             Text("Получить информацию о карте")
         }
 
         if (loading) {
-            CircularProgressIndicator()
+            CircularProgressIndicator(modifier = Modifier.padding(top = 16.dp))
         } else {
             cardInfo?.let {
+                Spacer(modifier = Modifier.height(100.dp))
                 Text(
-                    "Страна: ${it.country}, " +
-                            "Широта: ${it.country.latitude}, " +
-                            "Долгота: ${it.country.longitude}" +
-                            "Тип карты: ${it.type}, "
+                    text = "Информация о карте",
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontWeight = FontWeight.Bold, fontSize = 18.sp)
                 )
+                Text("Страна: ${it.country}")
+                Text("Широта: ${it.country.latitude}")
+                Text("Долгота: ${it.country.longitude}")
+                Text("Тип карты: ${it.type}")
+
+                Spacer(modifier = Modifier.height(14.dp))
+
                 Text(
-                    "Данные банка:" +
-                            "URL: ${it.bank.url}" +
-                            "Телефон: ${it.bank.phone}" +
-                            "Сайт: ${it.bank.name}" +
-                            "Город: ${it.bank.city}"
-                )
+                    "Данные банка:",
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                    )
+                Text("URL: ${it.bank.url}")
+                Text("Телефон: ${it.bank.phone}")
+                Text("Сайт: ${it.bank.name}")
+                Text("Город: ${it.bank.city}")
             }
         }
 
         errorMessage?.let { message ->
-            Text(text = message, color = Color.Red)
+            Text(text = message, color = Color.Red, modifier = Modifier.padding(top = 16.dp))
         }
     }
 }
+
